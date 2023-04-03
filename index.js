bdec21a443abcab41a13857501deab3d31221ebe
Sreedhar <56937351+Sreedharbot@users.noreply.github.com>
2023-04-03 22:23:09 +0800

    Web_Server_files

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
+
 const express = require('express');
-const server = express();
+const datastore = require('nedb');
+const app = express();
 
 port = 3000;
 
-server.listen(port,()=>console.log(`Server listening to - ${port}`));
-server.use(express.static('public'));
+
+app.listen(port, ()=>console.log(`Listening to Port - ${port}`));
+app.use(express.static('public2'));
+app.use(express.json({limit:'2mb'}));
+
+
+
+
+const database = new datastore('website.db');
+database.loadDatabase();
+
+app.get('/data',(request,response)=>{
+    database.find({},(err,data)=>{
+        if(err){
+            console.end();
+            return;
+        }
+        response.json(data);
+    });
+});
+
+app.post('/api',(request,response)=>{
+    //console.log(request.body);
+    const data = request.body;
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
+    database.insert(data);
+    console.log(database)
+    response.json({
+        timestamp2: timestamp,     
+    });
+});
